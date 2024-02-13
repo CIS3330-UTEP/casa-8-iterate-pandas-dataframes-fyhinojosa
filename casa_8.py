2a928281b772ea87e7f151d3cb8dc7d1fef12a3e
fyhinojosa <147429973+fyhinojosa@users.noreply.github.com>
2024-02-13 16:25:35 -0700

    Casa_8

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+df = pd.read_csv('big-mac-full-index.csv')
+
+# Using iterrows() method
+for i,r in df.iterrows():
+    print(r['currency_code'],
+          r['local_price'])
+
+# Using apply() method
+print(df.apply(lambda row: row["name"], axis = 1))
